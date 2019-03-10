b15567eae9c384135f40873f7a514da8c1b4779e
sungill <kdsko123@naver.com>
2019-03-09 22:29:53 -0500

    Add homework.cpp

---
+++ homework.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a,b,c;
+	printf("a = ");
+	scanf("%d",&a);
+	printf("b = ");
+	scanf("%d",&b);
+
+for(int i = a; i <= b; i++){
+c = c+i;
+}
+printf("from a to b = %d\n", c);
+return 0;
+}
+
